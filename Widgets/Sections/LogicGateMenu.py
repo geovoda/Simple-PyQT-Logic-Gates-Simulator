28b958ab98cc48e2dbef7581f026074694bc7765
unknown <ion.petriciuc@student.tuiasi.ro>
2022-12-06 18:22:47 +0200

    modificare toate butoane

--- Widgets/Sections/LogicGateMenu.py
+++ Widgets/Sections/LogicGateMenu.py
@@ -15,44 +15,28 @@ class LogicGateMenu(QWidget):
         button.clicked.connect(lambda: self.createLogicGate("INPUT_STREAM"))
         vbox.addWidget(button)
 
-        button = QPushButton("AND")
-        button.clicked.connect(lambda: self.createLogicGate("AND"))
-        button.setIcon(QtGui.QIcon('and.png'))
-        button.setIconSize(QtCore.QSize(50, 50))
-        vbox.addWidget(button)
-
-        button = QPushButton("NAND")
-        button.clicked.connect(lambda: self.createLogicGate("NAND"))
-        button.setIcon(QtGui.QIcon('nand.png'))
-        button.setIconSize(QtCore.QSize(50, 50))
-        vbox.addWidget(button)
-
-        button = QPushButton("OR")
-        button.clicked.connect(lambda: self.createLogicGate("OR"))
-        button.setIcon(QtGui.QIcon('or.png'))
-        button.setIconSize(QtCore.QSize(50, 50))
-        vbox.addWidget(button)
-
-        button = QPushButton("NOR")
-        button.clicked.connect(lambda: self.createLogicGate("NOR"))
-        button.setIcon(QtGui.QIcon('nor.png'))
-        button.setIconSize(QtCore.QSize(50, 50))
-        vbox.addWidget(button)
-
-        button = QPushButton("XOR")
-        button.clicked.connect(lambda: self.createLogicGate("XOR"))
-        button.setIcon(QtGui.QIcon('xor.png'))
-        button.setIconSize(QtCore.QSize(50, 50))
-        vbox.addWidget(button)
-
-        button = QPushButton("XNOR")
-        button.clicked.connect(lambda: self.createLogicGate("XNOR"))
-        button.setIcon(QtGui.QIcon('xnor.png'))
-        button.setIconSize(QtCore.QSize(50, 50))
-        vbox.addWidget(button)
-
         button = GateButton("AND", self)
         button.connect(lambda: self.createLogicGate("AND"))
+        vbox.addWidget(button)
+
+        button = GateButton("NAND", self)
+        button.connect(lambda: self.createLogicGate("OR"))
+        vbox.addWidget(button)
+
+        button = GateButton("OR", self)
+        button.connect(lambda: self.createLogicGate("AND"))
+        vbox.addWidget(button)
+
+        button = GateButton("NOR", self)
+        button.connect(lambda: self.createLogicGate("OR"))
+        vbox.addWidget(button)
+
+        button = GateButton("XNOR", self)
+        button.connect(lambda: self.createLogicGate("AND"))
+        vbox.addWidget(button)
+
+        button = GateButton("OR", self)
+        button.connect(lambda: self.createLogicGate("OR"))
         vbox.addWidget(button)
 
         self.setLayout(vbox)
